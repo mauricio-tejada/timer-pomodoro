35943bcc3bf74c4b5e2f0278f111b5081d8602e2
Mauricio <you@example.com>
2022-10-22 10:24:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/button"
+
+
+function App() {
+
+  return (
+    <>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='danger'/>
+      <Button variant='success'/>
+      <Button />
+    </>
+  )
+}
+
+export default App
